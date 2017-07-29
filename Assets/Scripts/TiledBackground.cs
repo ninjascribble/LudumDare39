3043f9b26aee8bad8bd5d92e6b3c4d4aed1bdc0d
Scott Grogan <scott@ninjascript.com>
2017-07-29 11:11:00 -0700

    Add scrolling background

---
+++ Assets/Scripts/TiledBackground.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiledBackground : MonoBehaviour {
+
+	public int textureSize = 128; // Must be a power of 2
+	public bool scaleHorizontally = true;
+	public bool scaleVertically = true;
+
+	// Use this for initialization
+	void Start () {
+		var newWidth = !scaleHorizontally ? 1 : Mathf.Ceil (Screen.width / (textureSize * PixelPerfectCamera.scale));
+		var newHeight = !scaleVertically ? 1 : Mathf.Ceil (Screen.height / (textureSize * PixelPerfectCamera.scale));
+
+		transform.localScale = new Vector3 (newWidth * textureSize, newHeight * textureSize, 1);
+
+		GetComponent<Renderer> ().material.mainTextureScale = new Vector3 (newWidth, newHeight, 1);
+	}
+}
